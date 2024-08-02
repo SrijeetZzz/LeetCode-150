f8c1f3ef9c80b86f0b0055e55e0a4b22aeff68da
SrijeetZzz <srijeetkarmakarnhs2@gmail.com>
2024-08-02 23:55:01 +0530

    day12

---
+++ ValidParenthesis.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean isValid(String s) {
+        HashMap<Character, Character> hm = new HashMap<>();
+        hm.put('(', ')');
+        hm.put('{', '}');
+        hm.put('[', ']');
+        Stack<Character> st = new Stack<>();
+        int i = 0;
+        while (i < s.length()) {
+            char current = s.charAt(i);
+            if (hm.containsKey(current)) {
+                st.add(current);
+            } else {
+                if (st.isEmpty() || current != hm.get(st.pop())) {
+                    return false;
+                }
+            }
+            i++;
+        }
+        
+        return st.isEmpty();
+    } 
+    public static void main(String[] args) {
+        String s = "{[]}";
+        System.out.println(isValid(s));
+    }
+}
